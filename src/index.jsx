1eebafaf11bee9e53915f8df0ca1103d438b5b6c
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-03-24 11:05:51 +0100

    set state

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import reduxPromise from 'redux-promise';
-import logger from 'redux-logger';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { createHistory as history } from 'history';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import reduxPromise from "redux-promise";
+import logger from "redux-logger";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { createHistory as history } from "history";
 
-import '../assets/stylesheets/application.scss';
+import "../assets/stylesheets/application.scss";
+
+const garageName =
+  prompt("What is your garage?") ||
+  `garage${Math.floor(10 + (Math.random() * 90)}`;
+
+const initialState = {
+  garage: garageName,
+  cars: [],
+};
 
 const reducers = combineReducers({
-  // key: reducer
+  garage: (state = null, action) => state,
+  cars: carsReducer
 });
 
 const middlewares = applyMiddleware(reduxPromise, logger);
+const store = createStore(reducers, initialState, middlewares);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={store>
     <Router history={history}>
-      <Switch>
-        TODO
-      </Switch>
+      <Switch>TODO</Switch>
     </Router>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
